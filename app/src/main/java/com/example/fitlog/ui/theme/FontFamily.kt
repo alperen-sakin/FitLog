003fb20c33d5f96eeb0c0c65d4f39ddab57469f4
alperen-sakin <azapkaan@gmail.com>
2026-02-20 03:26:35 +0100

    feat: Add Inter Medium font and new color
    - Add the Inter Medium font weight.
    - Introduce the `Natural100` color.

--- app/src/main/java/com/example/fitlog/ui/theme/FontFamily.kt
+++ app/src/main/java/com/example/fitlog/ui/theme/FontFamily.kt
@@ -7,6 +7,7 @@ import com.example.fitlog.R
 
 val Inter = FontFamily(
     Font(R.font.inter, FontWeight.Normal),
+    Font(R.font.inter_medium, FontWeight.Medium),
     Font(R.font.inter_bold, FontWeight.Bold),
     Font(R.font.inter_extrabold, FontWeight.ExtraBold),
     Font(R.font.inter_black, FontWeight.Black),
